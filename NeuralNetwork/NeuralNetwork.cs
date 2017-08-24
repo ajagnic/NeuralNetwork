9e003dfb42d4bd9509f2015dd61f19cad22e9ff5
Adrian Agnic <adrian.agnic@hotmail.com>
2017-08-24 11:32:26 -0700

    Start FeedForward method

--- NeuralNetwork/NeuralNetwork.cs
+++ NeuralNetwork/NeuralNetwork.cs
@@ -5,9 +5,9 @@ namespace NeuralNetwork
 {
     public class NeuralNetwork
     {
-        public int[] Layers;                    // ex. Layers = [2, 3, 3, 1]    ---INPUT EXAMPLE: [ 0.5, 1 ]
+        public int[] Layers;                    // ex. Layers = [2, 3, 3, 1]    ---INPUT EXAMPLE: [ 0.5, 1.0 ]
 
-        private float[][] Nodes;                // ex. Nodes = [currentLayer][nodes] = [i0][2] = [0][ 0.5, 1 ]   //REFERENCES LAYERS
+        private float[][] Nodes;                // ex. Nodes = [currentLayer][nodes] = [i0][2] = [0][ 0.5, 1.0 ]   //REFERENCES LAYERS
 
         private float[][][] Weights;            // ex. Weights = [''][''][prevNodes] = [i1][3][2]                //REFERENCES LAYERS, NODES
 
@@ -27,9 +27,27 @@ namespace NeuralNetwork
             CreateWeightMatrix();
         }//--------------------------------------------------------------------------------------
 
-        public void FeedForward()
+        /* We want to place the input values into the input layer, hence targeting Nodes[0]
+         *
+         */
+        public float[] FeedForward(float[] inputs)      // ex. inputs[ 0.5, 1.0 ]
         {
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                Nodes[0][i] = inputs[i];                // ex. Nodes [ [ 0.5, 1.0 ], [] ]
+            }
 
+            //------------------------------------
+            for (int i = 0; i < Layers.Length; i++)
+            {
+                //--------------------------------------
+                for (int j = 0; j < Nodes[i].Length; j++)
+                {
+
+                }
+            }
+
+            return null;//TEMP!!
         }
 
         public void MutateWeights()
@@ -39,7 +57,8 @@ namespace NeuralNetwork
 
         /* We have an amount of layers and we know how many nodes we want... But we need each node to reference information from inputs. So each node must be an array of numbers.
          * So we need an array of arrays, but we dont want to specify the array length for each... So we use Lists for the variable size and Add to them in the loop.
-         * Finally, we convert the List to a normal array. Jagged array is simply an array of arrays, symbolized with "[][]". */
+         * Finally, we convert the List to a normal array. Jagged array is simply an array of arrays, symbolized with "[][]".
+         */
         public void CreateNodeMatrix()
         {
             List<float[]> nodeList = new List<float[]>();
@@ -47,14 +66,15 @@ namespace NeuralNetwork
             for (int i = 0; i < Layers.Length; i++)         // ex. Layers[2, 3, 3, 1]
             {
                 nodeList.Add(new float[Layers[i]]);         // ex. nodeList = [ [x,x], [x,x,x], [x,x,x], [x] ]
-            }                                               // ex.w/input  [ [ 0.5, 1 ] , [x, x, x] , ...]
+            }                                               // ex.w/input  [ [ 0.5, 1.0 ] , [x, x, x] , ...]
 
             Nodes = nodeList.ToArray();                     // conversion from List<[]> to jagged array [][]
         }
 
         /* This will be similar to node matrix, but we go a level deeper to log the connections b/w each node, hence the 3D array "[][][]".
          * Start Layer loop at i=1 since the first layer has no weighted connections.
-         * We initialize the weights to random values, these will later be adjusted with mutations. */
+         * We initialize the weights to random values, these will later be adjusted with mutations.
+         */
         public void CreateWeightMatrix()
         {
             List<float[][]> weightList = new List<float[][]>();
