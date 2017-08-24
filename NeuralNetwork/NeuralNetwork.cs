237adbcfa61c73ca6577e34e9429b34ec7f2bc00
Adrian Agnic <adrian.agnic@hotmail.com>
2017-08-24 08:54:32 -0700

    Add project files.

---
+++ NeuralNetwork/NeuralNetwork.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace NeuralNetwork
+{
+    public class NeuralNetwork
+    {
+        public int[] Layers;                    // ex. Layers = [2, 3, 3, 1]
+
+        private float[][] Nodes;                // ex. Nodes = [currentLayer][nodes] = [0][2]
+
+        private float[][][] Weights;            // ex. Weights = [''][''][prevNodes] = [1][3][2]
+
+        public Random random = new Random();    //RNG
+
+        //========================================================================================
+        public NeuralNetwork(int[] layerInfo)
+        {
+            Layers = new int[layerInfo.Length];
+
+            for (int i = 0; i < layerInfo.Length; i++)
+            {
+                Layers[i] = layerInfo[i];
+            }
+
+            CreateNeuronMatrix();
+            CreateWeightMatrix();
+        }
+
+        public void FeedForward()
+        {
+
+        }
+
+        public void Query()
+        {
+
+        }
+
+        public void Train()
+        {
+
+        }
+
+        public void CreateNeuronMatrix()
+        {
+
+        }
+
+        public void CreateWeightMatrix()
+        {
+
+        }
+    }
+}
